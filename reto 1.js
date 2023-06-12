0d147c12c5467ca2ee1fb9b270089997a937b854
anudsa <anwardelgadosilva@gmail.com>
2023-06-11 21:36:10 -0600

    Primer reto

---
+++ reto 1.js
@@ -0,0 +1,16 @@
+//p1
+console.log("Parte 1");
+const fila=["Sofía","David","Juan"];
+console.log(fila);
+
+//p2
+console.log("Parte 2");
+fila.push("Sara","Agustin"); 
+fila.shift();
+console.log(fila);
+
+//p3
+console.log("Parte 3"); 
+fila.splice(1, 0, 'Renata');
+fila.push("Elena");
+console.log(fila);
